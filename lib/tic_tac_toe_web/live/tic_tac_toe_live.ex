70932dc764668d91c5dc25a6b5f9f3fe26949ba6
Brett Wise <brettdwise@gmail.com>
2020-08-16 10:59:49 -0400

    Co-locating live template

--- lib/tic_tac_toe_web/live/tic_tac_toe_live.ex
+++ lib/tic_tac_toe_web/live/tic_tac_toe_live.ex
@@ -6,23 +6,6 @@ defmodule TicTacToeWeb.TicTacToeLive do
     {:ok, assign(socket, :game_state, GameState.init_game())}
   end
 
-  def render(assigns) do
-    ~L"""
-    <div class="game-header">
-      <p>Player 1: <%= @game_state.player_1.name %> || Player 2: <%= @game_state.player_2.name %></p>
-      <h2>Game Status: <%= @game_state.game_status %></h2>
-      <h2><%= @game_state.message %></h2>
-    </div>
-    <div class="board">
-      <%= for number <- 1..9 do %>
-        <button <%= is_in_win_state?(@game_state.game_status) %> class="space" phx-click=<%= get_mark_string(number) %>>
-          <%= get_space_value(@game_state.board, number) %>
-        </button>
-      <% end %>
-    </div>
-    """
-  end
-
   def handle_event("mark_space_1", _, socket), do: update_game(socket, :space_1)
   def handle_event("mark_space_2", _, socket), do: update_game(socket, :space_2)
   def handle_event("mark_space_3", _, socket), do: update_game(socket, :space_3)
